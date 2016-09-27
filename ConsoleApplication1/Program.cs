9cb9b79b0a9af9824143017b86cb6f9824fa16b7
14ceuos041.ce <14ceuos041.ce@ddu.ac.in>
2016-09-27 17:27:02 +0530

    commit 1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database1Entities de = new Database1Entities();
+            Student s1 = new Student()
+            {
+                Id = 1,
+                Name = "Biren"
+            };
+            de.Students.Add(s1);
+            de.SaveChanges();
+
+
+            IQueryable<Student> query = from c in de.Students
+                        select c;
+
+            foreach(var a in query){
+                Console.WriteLine(a.Name);
+
+            }
+            Console.ReadLine();
+
+            de.Students.Remove(s1);
+            de.SaveChanges();
+
+            foreach (var a in query)
+            {
+                Console.WriteLine(a.Name);
+
+            }
+
+        }
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student()
+            {
+                Id = 4,
+                Name = "ABC"
+
+            };
+
+            DataClasses1DataContext cdc = new DataClasses1DataContext();
+            cdc.Students.InsertOnSubmit(s1);
+            cdc.SubmitChanges();
+
+            var q = cdc.GetTable<Student>().Where(i => i.Id == 4).Select(i=>i);
+
+            foreach(var a in q)
+            {
+                Console.WriteLine(a.Name);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
